4d541ea3d971d5206311de51c753e220af52cb27
ivanm21 <ivanmalyshenko@gmail.com>
2016-03-28 23:16:36 +0300

    Initial commit for RSG-11

---
+++ src/Domain/Entities/Houshold.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Houshold
+    {
+        public int UserGroupId { get; set; }
+        public string GroupName { get; set; }
+        public string Type { get; set; }
+    }
+}
