384ec5ec4d9fb4e649731b9201bad64e8ec23500
emohat1r <andreyws96@gmail.com>
2016-04-05 22:06:30 +0300

    Added semantic and tweaked gulp

---
+++ src/WebUI/gulp/configs/main.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  paths: {
+    entry: './js/index.js',
+    js: './Assets/js/**/*.js',
+    html: './Assets/html/**/*.html',
+    css: './Assets/css/**/*.css'
+    dist: './wwwroot',
+    images: './Assets/images/**/*'
+  },
+  output: {
+    js: 'js',
+    css: 'css',
+    images: 'images',
+    html: 'html'
+  },
+  production: false
+};
--- src/WebUI/gulpfile.js
+++ src/WebUI/gulpfile.js
@@ -1,45 +1,65 @@
-﻿/// <binding Clean='clean' />
+﻿ /// <binding Clean='clean' />
 "use strict";
 
 var gulp = require("gulp"),
-    rimraf = require("rimraf"),
-    concat = require("gulp-concat"),
-    cssmin = require("gulp-cssmin"),
-    uglify = require("gulp-uglify");
+  rimraf = require("rimraf"),
+  concat = require("gulp-concat"),
+  cssmin = require("gulp-cssmin"),
+  webpack = require('webpack-stream'),
+  uglify = require("gulp-uglify");
+
 
 var paths = {
-    webroot: "./wwwroot/"
+  webroot: "./wwwroot/"
 };
 
-paths.js = paths.webroot + "js/**/*.js";
+paths.js = "./Assets/js/**/*.js";
+paths.css = "./Assets/css/**/*.css";
+paths.libs = "./Libraries/**/*";
+paths.images = "./Assets/images/**/*";
 paths.minJs = paths.webroot + "js/**/*.min.js";
-paths.css = paths.webroot + "css/**/*.css";
 paths.minCss = paths.webroot + "css/**/*.min.css";
 paths.concatJsDest = paths.webroot + "js/site.min.js";
 paths.concatCssDest = paths.webroot + "css/site.min.css";
+paths.libsDest = paths.webroot + "lib";
+paths.imagesDest = paths.webroot + "images";
 
-gulp.task("clean:js", function (cb) {
-    rimraf(paths.concatJsDest, cb);
+gulp.task("clean:js", function(cb) {
+  rimraf(paths.concatJsDest, cb);
 });
 
-gulp.task("clean:css", function (cb) {
-    rimraf(paths.concatCssDest, cb);
+gulp.task("clean:css", function(cb) {
+  rimraf(paths.concatCssDest, cb);
 });
+
+gulp.task("min:js", function() {
+  return gulp.src([paths.js, "!" + paths.minJs], {
+      base: "."
+    })
+    .pipe(concat(paths.concatJsDest))
+    .pipe(uglify())
+    .pipe(gulp.dest("."));
+});
+
+gulp.task("min:css", function() {
+  return gulp.src([paths.css, "!" + paths.minCss])
+    .pipe(concat(paths.concatCssDest))
+    .pipe(cssmin())
+    .pipe(gulp.dest("."));
+});
+
+gulp.task("copy:libraries", function() {
+  return gulp.src(paths.libs, { base: './Libraries' })
+    .pipe(gulp.dest(paths.libsDest));
+});
+
+gulp.task("copy:images", function() {
+  return gulp.src(paths.images, { base: './Assets' })
+    .pipe(gulp.dest(paths.imagesDest));
+});
+
 
 gulp.task("clean", ["clean:js", "clean:css"]);
-
-gulp.task("min:js", function () {
-    return gulp.src([paths.js, "!" + paths.minJs], { base: "." })
-        .pipe(concat(paths.concatJsDest))
-        .pipe(uglify())
-        .pipe(gulp.dest("."));
-});
-
-gulp.task("min:css", function () {
-    return gulp.src([paths.css, "!" + paths.minCss])
-        .pipe(concat(paths.concatCssDest))
-        .pipe(cssmin())
-        .pipe(gulp.dest("."));
-});
-
 gulp.task("min", ["min:js", "min:css"]);
+gulp.task("copy", ["copy:libraries", "copy:images"]);
+gulp.task("default", ["min", "clean"]);
