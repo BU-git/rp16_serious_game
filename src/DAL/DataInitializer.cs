ed0f7edffcc6577460331b79de7348953cab3a37
emohat1r <andreyws96@gmail.com>
2016-04-12 23:01:54 +0300

    Deleted unused views

--- src/DAL/DataInitializer.cs
+++ src/DAL/DataInitializer.cs
@@ -64,7 +64,8 @@ namespace DAL
 
         private async Task CreateUsersAsync()
         {
-            var user1 = await _userManager.FindByEmailAsync("Admin@admin.com");
+            var user1 =
+                await _userManager.FindByEmailAsync(new UpperInvariantLookupNormalizer().Normalize("Admin@admin.com"));
             if (user1 == null)
             {
                 ApplicationUser admin = new ApplicationUser() { UserName = "Admin@admin.com", Name = "Admin1", Email = "Admin@admin.com", Gender = Gender.Male };
