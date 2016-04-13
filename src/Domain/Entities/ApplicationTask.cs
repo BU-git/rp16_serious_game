0d9b1d09f6e2cccf8c31839094af207eeb2521f2
ivanm21 <ivanmalyshenko@gmail.com>
2016-04-13 20:55:32 +0300

    Adde missed Task entity

---
+++ src/Domain/Entities/ApplicationTask.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class ApplicationTask
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Text { get; set; }
+        public DateTime Recurency { get; set; }
+        public int Coins { get; set; }
+
+        public List<UserTask> UserTasks { get; set; } 
+    }
+}
