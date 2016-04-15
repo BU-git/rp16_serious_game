5a6b02f63c06060cb2d802c4adbd1b510dd64aaa
emohat1r <andreyws96@gmail.com>
2016-04-15 14:08:15 +0300

    Fixed minor issues with login

---
+++ src/WebUI/ViewModels/Manage/PersonalInformationViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace WebUI.ViewModels.Manage
+{
+    public class PersonalInformationViewModel
+    {
+
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public string Phone { get; set; }
+
+        public int Index { get; set; }
+        public string ZipCode { get; set; }
+        public string Street { get; set; }
+        public string Country { get; set; }
+        public string Region { get; set; }
+        public string City { get; set; }
+        public string BuildingNumber { get; set; }
+    }
+}
